8942ea4af10143808fca968ce2bc9d70ce54d71b
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-10-20 22:41:39 +0800

    Cleaning up code

--- simple-pomodoro/src/Containers/App.js
+++ simple-pomodoro/src/Containers/App.js
@@ -1,10 +1,11 @@
-import { duration } from '@material-ui/core';
+// Importing React using Webpock
 import React, { Component } from 'react';
+// Importing styling for App Parent Component
 import './App.css';
-// Components
-import Controller from './Components/Controls';
-import SliderInput from './Components/SliderInput';
-import TimerDisplay from './Components/TimerDisplay';
+// Importing Components
+import Controller from '../Components/Controls';
+import SliderInput from '../Components/SliderInput';
+import TimerDisplay from '../Components/TimerDisplay';
 
 // APP Class
 class App extends Component {
@@ -21,23 +22,22 @@ class App extends Component {
             displayTime: 6,
             button: true,
             cycle:0
-
         }
-        console.log(this.state)
     }
 
     forceUpdateHandler = () => {
         this.forceUpdate();
     };
 
+    // Getting the value of the sliderInput to change the state of the timer
     onfocusTimeChange = (event) => {
         this.setState({
             focusTime: event.target.ariaValueNow,
             resetFocusTime: event.target.ariaValueNow,
             displayTime: event.target.ariaValueNow * 60
         }, () => {
-            console.log(this.displayTime)
-            this.forceUpdateHandler()
+            console.log(this.state.displayTime);
+            this.forceUpdateHandler();
         })
     }
 
@@ -46,46 +46,53 @@ class App extends Component {
             restTime: event.target.ariaValueNow,
             resetRestTime: event.target.ariaValueNow
         }, () => {
-            console.log(this.displayTime)
-            this.forceUpdateHandler()
+            console.log(this.state.displayTime);
+            this.forceUpdateHandler();
         })
     }
 
+    // Creating a function for the countdown timer
     startTimer = (duration) => {
-        console.log('TIMER HAS STARTED')
-        let timer = duration * 60;
-        let countDown = duration * 60
+        console.log('The timer has started: ', this.state.phase)
 
+        // Creating separate variables for display and for state change
+        let timer = duration * 60;
+        let countDown = duration * 60;
+
+        // Using setInterval() method to create a countdown
         const time = setInterval(() => {
             if (this.state.status === true) {
                 timer--;
                 countDown--;
 
-
+                // Checking which phase the pomodoro is in: FOCUS
                 if (this.state.phase === 'focus') {
-                    this.setState({ displayTime: timer })
+                    this.setState({ displayTime: timer });
 
                     this.setState({ focusTime: countDown / 60 },
                         () => {
-                            console.log('NEW FT: ', this.state.focusTime)
-                            this.forceUpdateHandler()
-                        })
+                            console.log('Updated focusTime: ', this.state.focusTime.toFixed(2));
+                            this.forceUpdateHandler();
+                        });
 
+                // Checking which phase the pomodoro is in: REST
                 } else if (this.state.phase === 'rest') {
-                    this.setState({ displayTime: timer })
+                    this.setState({ displayTime: timer });
 
                     this.setState({ restTime: countDown / 60 },
                         () => {
-                            console.log('NEW RT: ', this.state.restTime)
-                            this.forceUpdateHandler()
-                        })
+                            console.log('Updated restTime: ', this.state.restTime.toFixed(2));
+                            this.forceUpdateHandler();
+                        });
                 }
             }
 
+            // If logic to capture PAUSE state
             if (this.state.status === false) {
                 clearInterval(time);
             }
 
+            // If logic to capture TIMER DONE
             if (timer <= 0) {
                 clearInterval(time);
             }
@@ -93,18 +100,19 @@ class App extends Component {
     }
 
     startTimerRest = (durationFocus, durationRest) => {
-
+            // Using setTimeout() to delay the REST Phase
             const restTime = setTimeout(() => {
+                // If logic to check if the timer is still running
                 if (this.state.status === true) {
                     this.startTimer(durationRest)
-                    this.setState({ phase: 'rest' }, () => console.log(this.state.phase))
+                    // Changing the phase state to REST
+                    this.setState({ phase: 'rest' }, () => console.log('The timer has started: ', this.state.phase))
 
                     const alerta = setTimeout(() => {
-                        alert('TIMES UP!')
-
-
+                        // Displaying a pop-up to alert end of timer
+                        alert('Good Job! You finished a cycle!')
+                        // Enabling button again using button state = True
                         this.setState({ button: true }, () => console.log(this.state.button))
-
                         this.setState({
                             focusTime: this.state.resetFocusTime,
                             restTime: this.state.resetRestTime,
